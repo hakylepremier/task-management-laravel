436906eb2c4226393e0e2fdd968491a3627e29c5
Humphrey Yeboah <haky@haky.com>
2023-12-04 23:28:23 +0000

    testing the same feature again

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're currently logged in!") }}
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
