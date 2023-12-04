839c28dcc0e538f9d92918f8aa044cdca1168347
Humphrey Yeboah <haky@haky.com>
2023-12-04 22:53:52 +0000

    fix: make category description field nullable

--- database/migrations/2023_12_03_125944_create_categories_table.php
+++ database/migrations/2023_12_03_125944_create_categories_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             );
             $table->text(
                 'description'
-            );
+            )->nullable();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
